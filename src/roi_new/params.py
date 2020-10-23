d69b77cc33254cccd796176cee759694090daf1a
Daniel Woschée <daniel.woschee@physik.lmu.de>
2020-10-23 15:08:51 +0200

    Started with new ROIs

---
+++ src/roi_new/params.py
@@ -0,0 +1,75 @@
+from threading import RLock
+
+import numpy as np
+
+from ..util import make_uid
+
+
+class RoiParameters:
+    __slots__ = ('__lock', '__id', '__name')
+    def __init__(self, name=None):
+        self.__lock = RLock()
+        self.__id = make_uid(self)
+        self.__name = name
+
+    @property
+    def lock(self):
+        return self.__lock
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def name(self):
+        with self.__lock:
+            return self.__name
+
+    @name.setter
+    def name(self, new):
+        with self.__lock:
+            if new:
+                self.__name = new
+            else:
+                self.__name = None
+
+    def props(self, **par_individual):
+        """Get the (technical) properties of the ROI.
+
+        Override this when subclassing RoiParameters.
+        `par_individual` holds the individual parameters of the given ROI.
+        This method should return an instance of RoiProperties or
+        of an subclass thereof.
+        """
+        raise NotImplementedError
+
+
+class RoiProperties:
+    """Provider of (techincal) ROI properties.
+
+    The properties are calculated based on the coordinates
+    just-in-time. Since many RoiProperties instances may consume
+    a significant amount of memory, this class is intended to
+    be used as throw-away class.
+
+    Once calculated, the properties should not be changed any more.
+    ROI properties should only be accessed through the
+    properties definded by this class (or its subclasses) to ensure
+    that just-in-time calculation is invoked, if necessary.
+
+    This class is intended to be sub-classed.
+    """
+    __slots__ = ()
+    def __init__(self):
+        self._coords = None
+        self._area = None
+
+
+    @property
+    def coords(self):
+        return self._coords
+
+
+
+
+
---
+++ src/roi_new/props.py
@@ -0,0 +1,80 @@
+from threading import RLock
+from weakref import WeakSet
+
+from ..util import make_uid
+
+class RoiLabel:
+    """Label class for ROIs.
+
+    name -- str, human-readable description
+    color -- default color for visual representation
+    rois -- WeakSet of ROIs having this label
+    groups -- WeakSet of RoiGroups having this label
+    """
+    __slots__ = ('__lock', '__id', '__name', '__color', '__rois', '__groups')
+    def __init__(self, name=None, color=None, rois=None, groups=None):
+        self.__lock = RLock()
+        self.__id = make_uid(self)
+        self.__name = name
+        self.__color = color
+        self.__rois = WeakSet()
+        self.__groups = WeakSet()
+
+    @property
+    def lock(self):
+        return self.__lock
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def name(self):
+        with self.__lock:
+            return self.__name
+
+    @name.setter
+    def name(self, new):
+        with self.__lock:
+            if new:
+                self.__name = new
+            else:
+                self.__name = None
+
+    @property
+    def color(self):
+        with self.__lock:
+            return self.__color
+
+    @color.setter
+    def color(self, new):
+        if not new:
+            new = None
+        with self.__lock:
+            self.__color = new
+
+    @property
+    def rois(self):
+        with self.__lock:
+            return self.__rois.copy()
+
+    def add_rois(self, *rois):
+        with self.__lock:
+            self.__rois.update(rois)
+
+    def drop_rois(self, *rois):
+        with self.__lock:
+            self.__rois.difference_update(rois)
+
+    @property
+    def groups(self):
+        with self.__lock:
+            return self.__groups.copy()
+
+    def add_groups(self, *groups):
+        with self.__lock:
+            self.__groups.update(groups)
+
+    def drop_groups(self, *groups):
+        with self.__lock:
+            self.__groups.difference_update(groups)
